cd665d21ad46314931ddcb1b4442c07a5fe2dad0
Josh Mu <hello@joshmu.dev>
2021-01-21 10:58:23 +1000

    fix(calculator): fix calc logic

--- src/pages/Calculator.vue
+++ src/pages/Calculator.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Calculator</h1>
   <div class="calculator-container">
-    <div v-if="total === 0" class="display">{{ curr }} {{ operator }}</div>
+    <div v-if="curr > 0 || total === 0" class="display">{{ curr }}</div>
     <div v-else class="display">{{ total }}</div>
     <div class="controls" @click="keypress">
       <div class="key">7</div>
@@ -24,8 +24,6 @@
 </template>
 
 <script>
-// todo: does not support multi operations
-// todo: display isn't correct
 // todo: support C clear function
 
 import { ref } from 'vue'
@@ -37,54 +35,64 @@ export default {
     const total = ref(0)
     const operator = ref(null)
 
+
+    // symbol is pressed
+    const operation = selectedOperation => {
+      // if operator exists
+      if (operator.value) {
+        prev.value = calc(operator.value)
+      } else {
+        prev.value = curr.value
+      }
+
+      // update
+      curr.value = 0
+      total.value = prev.value
+      operator.value = selectedOperation
+
+      if (operator.value === '=') {
+        operator.value = null
+        total.value = prev.value
+        prev.value = 0
+      }
+    }
+
+    const calc = (operation) => {
+      let answer = 0;
+      switch (operation) {
+        case '*':
+          answer = prev.value * curr.value
+          break
+        case '/':
+          answer = prev.value / curr.value
+          break
+        case '-':
+          answer = prev.value - curr.value
+          break
+        case '+':
+          answer = prev.value + curr.value
+          break
+        default:
+          return
+      }
+      return answer
+    }
+
+    // type a number
+    const num = selectedNum => {
+      if (curr.value === 0) {
+        curr.value = Number(selectedNum)
+      } else {
+        curr.value = Number(curr.value + selectedNum)
+      }
+    }
+
     const keypress = e => {
       const val = e.target.innerText
       if (symbols.includes(val)) {
         operation(val)
       } else {
         num(val)
-      }
-    }
-
-    const operation = selectedOperation => {
-      if (operator.value !== null) {
-        calc()
-      }
-      // store values
-      prev.value = curr.value
-      curr.value = 0
-      operator.value = selectedOperation
-    }
-
-    const calc = () => {
-      switch (operator.value) {
-        case '*':
-          total.value = prev.value * curr.value
-          break
-        case '/':
-          total.value = prev.value / curr.value
-          break
-        case '-':
-          total.value = prev.value - curr.value
-          break
-        case '+':
-          total.value = prev.value + curr.value
-          break
-        case '=':
-          operation(operator.value)
-          break
-        default:
-          return
-      }
-      prev.value = total.value
-      curr.value = 0
-    }
-
-    const num = selectedNum => {
-      if (curr.value === 0) {
-        curr.value = Number(selectedNum)
-      } else {
-        curr.value = Number(curr.value.toString() + selectedNum)
       }
     }
 
